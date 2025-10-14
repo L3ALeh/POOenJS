95851821225ca0652a66b9df6acbb86f8dad5ee9
Léa <lleheron.ledantec@gmail.com>
2025-10-14 14:41:52 +0200

    Initial commit

---
+++ module1/demo1/test.js
@@ -0,0 +1,10 @@
+import {Client} from "./Client.js";
+import {Voiture} from "./Voiture.js";
+
+
+//gestion import Settings -> Editor -> Code style -> Js -> Imports -> Always
+
+let client  = new Client();
+console.log(client);
+let v = new Voiture();
+console.log(v);
