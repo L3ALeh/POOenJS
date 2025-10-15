65aa12e693f8bc721151f60495ac1893cc346690
Léa <lleheron.ledantec@gmail.com>
2025-10-15 15:46:04 +0200

    calendar

--- module2/tpCalendrier/script.js
+++ module2/tpCalendrier/script.js
@@ -1,4 +1,4 @@
-import {Calendar} from "../../../../Downloads/Fichiers de correction-20251015/renderer/Calendar.js";
+import {Calendar} from './renderer/Calendar.js';
 
 window.onload = function () {
     new Calendar("datePicker", "calendar")
